97670b9d4a61c187cff6a1dc0f139dc7cad5a47c
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2025-01-07 08:45:57 -0300

    Update Card.jsx

--- src/components/atoms/Card.jsx
+++ src/components/atoms/Card.jsx
@@ -1,25 +1,38 @@
 import React from "react";
-import Card from "../atoms/Card";
-import projectsData from "../amots/CardsData"; // Archivo con datos de proyectos
 
-const Proyects = () => {
-    const containerStyle = {
-        display: "flex",
-        justifyContent: "space-around",
-        alignItems: "flex-start",
-        backgroundColor: "#fff", // Fondo diferente si aplica
-        padding: "40px 20px",
-        flexWrap: "wrap",
-        gap: "20px",
+const Card = ({ icon, title, description }) => {
+    const cardStyle = {
+        textAlign: "center",
+        width: "300px",
+        margin: "20px",
+    };
+
+    const iconStyle = {
+        fontSize: "3rem",
+        color: "#76C893",
+        marginBottom: "10px",
+    };
+
+    const titleStyle = {
+        fontSize: "1.5rem",
+        fontWeight: "bold",
+        color: "#333",
+        marginBottom: "10px",
+    };
+
+    const descriptionStyle = {
+        fontSize: "1rem",
+        color: "#555",
+        lineHeight: "1.6",
     };
 
     return (
-        <div style={containerStyle}>
-            {projectsData.map((project, index) => (
-                <Card key={index} icon={project.icon} title={project.title} description={project.description} />
-            ))}
+        <div style={cardStyle}>
+            <i className={icon} style={iconStyle}></i>
+            <h3 style={titleStyle}>{title}</h3>
+            <p style={descriptionStyle}>{description}</p>
         </div>
     );
 };
 
-export default Proyects;
+export default Card;
