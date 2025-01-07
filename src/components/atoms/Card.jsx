414f1a4916d3df80e1799dd7757ca886a741a715
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2025-01-06 21:17:21 -0300

    Update Card.jsx

--- src/components/atoms/Card.jsx
+++ src/components/atoms/Card.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Letras from "../atoms/Typo";
 
 const Card = ({ icon, title, description }) => {
-    const valueStyle = {
+    const cardStyle = {
         textAlign: "center",
-        width: "100%",
-        maxWidth: "300px",
-        margin: "10px 0",
+        width: "300px",
+        margin: "20px",
         padding: "20px",
         border: "1px solid #ddd",
         borderRadius: "8px",
@@ -17,11 +15,10 @@ const Card = ({ icon, title, description }) => {
     const iconStyle = {
         fontSize: "3rem",
         color: "#76C893",
-        marginBottom: "15px",
+        marginBottom: "10px",
     };
 
     const titleStyle = {
-        fontFamily: Letras.textFont,
         fontSize: "1.5rem",
         fontWeight: "bold",
         color: "#333",
@@ -29,14 +26,13 @@ const Card = ({ icon, title, description }) => {
     };
 
     const descriptionStyle = {
-        fontFamily: Letras.textFont,
         fontSize: "1rem",
         color: "#555",
         lineHeight: "1.6",
     };
 
     return (
-        <div style={valueStyle}>
+        <div style={cardStyle}>
             <i className={icon} style={iconStyle}></i>
             <h3 style={titleStyle}>{title}</h3>
             <p style={descriptionStyle}>{description}</p>
