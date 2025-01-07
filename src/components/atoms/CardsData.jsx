538fbc61820b75ada3853527cfbb94941f82ce83
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2025-01-06 21:15:55 -0300

    Create CardsData.jsx

---
+++ src/components/atoms/CardsData.jsx
@@ -0,0 +1,41 @@
+
+const cardsData = [
+    {
+        icon: "fas fa-seedling",
+        title: "Sostenibilidad",
+        description:
+            "Cada tallado contribuye a reforestar nuestro planeta, plantando un árbol por cada pieza creada.",
+    },
+    {
+        icon: "fas fa-hands",
+        title: "Artesanía",
+        description:
+            "Diseños únicos y personalizados, hechos a mano con materiales de calidad.",
+    },
+    {
+        icon: "fas fa-palette",
+        title: "Personalización",
+        description:
+            "Ofrecemos opciones personalizadas para reflejar tu estilo y personalidad.",
+    },
+    {
+        icon: "fas fa-leaf",
+        title: "Innovación",
+        description:
+            "Estamos constantemente explorando nuevas formas de combinar tradición y modernidad.",
+    },
+    {
+        icon: "fas fa-heart",
+        title: "Pasión",
+        description:
+            "Ponemos el corazón en cada detalle, asegurando que cada pieza cuente una historia única.",
+    },
+    {
+        icon: "fas fa-star",
+        title: "Calidad",
+        description:
+            "Utilizamos materiales y procesos de alta calidad para garantizar productos duraderos y excepcionales.",
+    },
+];
+
+export default CardsData;
