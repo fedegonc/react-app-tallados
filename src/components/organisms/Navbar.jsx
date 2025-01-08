ef318cbc3de0d9ea94d29881f02a18786fae43e9
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2025-01-08 10:18:12 -0300

    Update Navbar.jsx

--- src/components/organisms/Navbar.jsx
+++ src/components/organisms/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
     top: 0,
     left: 0,
     width: "100%",
-    backgroundColor: "rgba(0, 0, 0, 0.8)", // Semitransparente
+    backgroundColor: "rgba(0, 0, 0, 0.1)", // Semitransparente
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
