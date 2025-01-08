7dd41ae4566977ba0f0356f1fc2586c053ed5788
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2025-01-08 10:05:14 -0300

    Update Banner.jsx

--- src/components/atoms/Banner.jsx
+++ src/components/atoms/Banner.jsx
@@ -21,7 +21,7 @@ const Banner = () => {
         left: 0,
         width: "100%",
         height: "100%",
-        backgroundColor: "rgba(0, 0, 0, 0.5)", // Superposición oscura
+        backgroundColor: "rgba(0, 0, 0, 0.2)", // Superposición oscura
     };
 
     const contentStyle = {
